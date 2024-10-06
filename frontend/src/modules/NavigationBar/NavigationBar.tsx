8bba045f58a8d099676b71d9d111282791728eb7
CristhianAc <cristhian_agamez@outook.com>
2024-10-05 23:15:27 -0500

    Refactor NavigationBar component and its related files

--- frontend/src/modules/NavigationBar/NavigationBar.tsx
+++ frontend/src/modules/NavigationBar/NavigationBar.tsx
@@ -10,14 +10,19 @@ const NavigationBar = () => {
     <div>
       <ToggleAsideButton isOpened={isOpened} setIsOpened={setIsOpened} />
       <div
-        className={` bg-black w-[100vw] h-[100vh] fixed z-50 transition-all animate-duration-800 ${
+        className={` bg-black/30 w-[100vw] h-[100vh] fixed z-50 transition-all animate-duration-800 ${
           isOpened ? " opacity-100" : "opacity-0"
         }`}
       ></div>
       <nav
-        className={` w-72 h-[100vh] bg-[var(--md-sys-color-inverse-primary)] fixed z-[100] transition-all ${isOpened ? " left-0" : " -left-72"}`}
+        className={` w-64 h-[100vh] bg-[var(--md-sys-color-inverse-primary-transparent)] fixed z-[100] transition-all ${
+          isOpened ? " left-0" : " -left-64"
+        }
+         flex flex-col justify-around items-center gap-20 py-64`}
       >
-
+        <a href="/">Home</a>
+        <a href="/dashboard">Dashboard</a>
+        <a href="/about">About</a>
       </nav>
     </div>
   );
