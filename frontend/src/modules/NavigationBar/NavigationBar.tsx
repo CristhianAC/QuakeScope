557c7e7508b708b313883ffa32cb8aad5f5031de
CristhianAc <cristhian_agamez@outook.com>
2024-10-05 19:15:25 -0500

    Refactor NavigationBar component and its related files

--- frontend/src/modules/NavigationBar/NavigationBar.tsx
+++ frontend/src/modules/NavigationBar/NavigationBar.tsx
@@ -1,16 +1,26 @@
+import React, { useState } from "react";
+import "./NavigationBar.css";
 
-
+import ToggleAsideButton from "./ToggleAsideButton";
 
 const NavigationBar = () => {
-    
-    return( 
-        <nav>
-        
-        <ul className="flex justify-between items-center">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-        </ul>
-        </nav>)
-    }
-    export default NavigationBar;
+  const [isOpened, setIsOpened] = useState(false);
+
+  return (
+    <div>
+      <ToggleAsideButton isOpened={isOpened} setIsOpened={setIsOpened} />
+      <div
+        className={` bg-black w-[100vw] h-[100vh] fixed z-50 transition-all animate-duration-800 ${
+          isOpened ? " opacity-100" : "opacity-0"
+        }`}
+      ></div>
+      <nav
+        className={` w-72 h-[100vh] bg-[var(--md-sys-color-inverse-primary)] fixed z-[100] transition-all ${isOpened ? " left-0" : " -left-72"}`}
+      >
+
+      </nav>
+    </div>
+  );
+};
+
+export default NavigationBar;
